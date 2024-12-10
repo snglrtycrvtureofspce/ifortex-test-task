ab4868982344db65d60078add5a07e71a47e1512
snglrtycrvtureofspce <asp1rantzenevich@gmail.com>
2024-12-10 17:27:50 +0300

    #

---
+++ TestTask/Services/Implementations/AuthorService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations;
+
+public class AuthorService(ApplicationDbContext context) : IAuthorService
+{
+    public async Task<Author> GetAuthor()
+    {
+        return await context.Books
+            .OrderByDescending(b => b.Title.Length)
+            .ThenBy(b => b.AuthorId)
+            .Select(b => b.Author)
+            .FirstOrDefaultAsync();
+    }
+
+    public async Task<List<Author>> GetAuthors()
+    {
+        return await context.Authors
+            .Where(a => a.Books.Count(b => b.PublishDate.Year > 2015) % 2 == 0)
+            .ToListAsync();
+    }
+}
---
+++ TestTask/Services/Implementations/BookService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations;
+
+public class BookService(ApplicationDbContext context) : IBookService
+{
+    public async Task<Book> GetBook()
+    {
+        return await context.Books
+            .OrderByDescending(b => b.Price * b.QuantityPublished)
+            .FirstOrDefaultAsync();
+    }
+
+    public async Task<List<Book>> GetBooks()
+    {
+        var albumSabatonPublishDate = new DateTime(2012, 5, 25); // в wiki две разные даты (25 мая и 22 мая)
+        
+        return await context.Books
+            .Where(b => b.Title.Contains("Red") && b.PublishDate > albumSabatonPublishDate)
+            .ToListAsync();
+    }
+}
